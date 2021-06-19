bb344692024c16661fb0fffde004ff69b33bf7f2
rastiQ-ntb <rasto.balcercik@gmail.com>
2021-06-19 22:17:57 +0200

    posts minor stuff

--- src/endpoints/posts.js
+++ src/endpoints/posts.js
@@ -1,13 +1,18 @@
 import { Post } from '../models/post.js'
 
-export const getPost = (userID, postID) => {
+export const getPost = (postID) => {
+    return new Promise((resolve, reject) => {
+        Post.findOne({_id: postID}).then(post => {
+            if(post) resolve(post)
+            else reject('no_post_found')
+        })
+    })
+}
+
+export const createPost = (content) => {
 
 }
 
-export const createPost = (posterID, content) => {
-
-}
-
-export const deletePost = (userID, postID) => {
+export const deletePost = (postID) => {
 
 }
